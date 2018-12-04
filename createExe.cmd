5a9aad1e3048b2fe5836d1b67a1f2e28cb987bab
furkankykc <furkankykc@gmail.com>
2018-12-04 15:57:35 +0300

    improove performance

--- createExe.cmd
+++ createExe.cmd
@@ -1,6 +1,2 @@
-<<<<<<< Updated upstream
 pyinstaller -w -F --icon=mail.ico main.py -n Mailler
 pyinstaller -F --icon=mail.ico main.py -n Mailler-Debug
-=======
-pyinstaller -w -F --icon=mail.ico main.py
->>>>>>> Stashed changes

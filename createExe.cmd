e3b22a3222bb9f29eb1ff108983c1885a2c2c789
furkankykc <furkankykc@gmail.com>
2018-12-04 15:56:09 +0300

    some mirror bugs fixed

--- createExe.cmd
+++ createExe.cmd
@@ -1,2 +1,6 @@
+<<<<<<< Updated upstream
 pyinstaller -w -F --icon=mail.ico main.py -n Mailler
 pyinstaller -F --icon=mail.ico main.py -n Mailler-Debug
+=======
+pyinstaller -w -F --icon=mail.ico main.py
+>>>>>>> Stashed changes
